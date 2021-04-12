feb826702eeae559cab2fbad098478c66ea96658
flybizitdev <flybizit@gmail.com>
2021-04-12 06:23:51 +0530

    'InitialCommit'

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x76, 0x14, 0x12, 0x74, 0x52, 0x45, 0x74, 0x52, 0x12, 0x24, 0x00, 0x11, 0x13, 0x18, 0x01, 0x19 } };
+}
